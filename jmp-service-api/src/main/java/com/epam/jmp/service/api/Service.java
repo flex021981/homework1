ad52af09605527168251ee8e6f268d6d98c249fd
Aleksei_Zakharov <aleksei.zakharov@clarivate.com>
2023-04-09 10:35:17 +0300

    Task9. Add Service interface to jmp-service-api with :     - void subscribe(BankCard)     - Optional<Subscription> getSubscriptionByBankCardNumber(String)     - List<User> getAllUsers();

---
+++ jmp-service-api/src/main/java/com/epam/jmp/service/api/Service.java
@@ -0,0 +1,14 @@
+package com.epam.jmp.service.api;
+
+import com.epam.jmp.dto.BankCard;
+import com.epam.jmp.dto.Subscription;
+import com.epam.jmp.dto.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Service {
+    void subscribe(BankCard bankCard);
+    Optional<Subscription> getSubscriptionByBankCardNumber(String number);
+    List<User> getAllUsers();
+}
